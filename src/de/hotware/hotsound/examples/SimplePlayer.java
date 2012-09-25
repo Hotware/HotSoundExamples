8a725fcc97d67ad39dde31bfb0e7904c4f73a0c1
Martin Braun <martinbraun123@aol.com>
2012-09-25 14:09:39 +0200

    changed

--- src/de/hotware/hotsound/examples/SimplePlayer.java
+++ src/de/hotware/hotsound/examples/SimplePlayer.java
@@ -9,19 +9,25 @@ import de.hotware.hotsound.audio.player.IMusicPlayer.SongInsertionException;
 import de.hotware.hotsound.audio.player.IPlaybackListener;
 import de.hotware.hotsound.audio.player.StreamMusicPlayer;
 
-
+/**
+ * Player that plays on the command line and it's 41 lines long
+ * 
+ * @author Martin Braun
+ */
 public class SimplePlayer {
-	
-	public static void main(String[] args) throws MalformedURLException, SongInsertionException, InterruptedException {
+
+	public static void main(String[] args) throws MalformedURLException,
+			SongInsertionException,
+			InterruptedException {
 		if(args.length >= 1) {
 			IMusicPlayer player = new StreamMusicPlayer(new IPlaybackListener() {
-	
+
 				@Override
 				public void onEnd(PlaybackEndEvent pEvent) {
 					System.out.println("Playback ended");
 					System.exit(1);
 				}
-				
+
 			});
 			player.insert(new BasicSong(new File(args[0])));
 			player.startPlayback();
