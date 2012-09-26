1025452d2c3de15b0c5a4ca834c9d38a81fa4001
Martin Braun <martinbraun123@aol.com>
2012-09-26 14:18:53 +0200

    minor

--- src/de/hotware/hotsound/examples/SimplePlayer.java
+++ src/de/hotware/hotsound/examples/SimplePlayer.java
@@ -1,7 +1,7 @@
 package de.hotware.hotsound.examples;
 
 import java.io.File;
-import java.net.MalformedURLException;
+import java.io.IOException;
 
 import de.hotware.hotsound.audio.player.BasicSong;
 import de.hotware.hotsound.audio.player.IMusicPlayer;
@@ -15,8 +15,8 @@ import de.hotware.hotsound.audio.player.StreamMusicPlayer;
  */
 public class SimplePlayer {
 
-	public static void main(String[] args) throws MalformedURLException,
-			SongInsertionException {
+	public static void main(String[] args) throws SongInsertionException,
+			IOException {
 		if(args.length >= 1) {
 			IMusicPlayer player = new StreamMusicPlayer();
 			player.insert(new BasicSong(new File(args[0])));
