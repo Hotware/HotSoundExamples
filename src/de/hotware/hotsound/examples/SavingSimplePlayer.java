833b878515e6b7241fff0e04e71b6ca24c95b01d
Martin Braun <martinbraun123@aol.com>
2012-09-27 18:10:53 +0200

    bug fix

--- src/de/hotware/hotsound/examples/SavingSimplePlayer.java
+++ src/de/hotware/hotsound/examples/SavingSimplePlayer.java
@@ -3,7 +3,9 @@ package de.hotware.hotsound.examples;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import de.hotware.hotsound.audio.data.SavingAudioDevice;
 import de.hotware.hotsound.audio.player.BasicSong;
@@ -17,6 +19,7 @@ public class SavingSimplePlayer {
 	
 	public static void main(String[] args) throws MusicPlayerException, MalformedURLException, InterruptedException {
 		if(args.length >= 2) {
+			ExecutorService service = Executors.newSingleThreadExecutor();
 			//multithreading because of blocking behaviour
 			IMusicPlayer player = new StreamMusicPlayer(new IMusicListener() {
 
@@ -25,13 +28,15 @@ public class SavingSimplePlayer {
 					System.out.println("stopped");
 				}
 				
-			}, Executors.newSingleThreadExecutor());
+			}, service);
 			player.insert(new BasicSong(new URL(args[0])), new SavingAudioDevice(new File(args[1])));
 			player.start();
 			//wait 10 seconds (equals approx. 10 seconds of saved audio)
 			Thread.sleep(10000);
 			//always stop for bug avoidance in saving the audiofile
 			player.stop();
+			service.shutdown();
+			service.awaitTermination(1000, TimeUnit.MILLISECONDS);
 		}
 	}
 
--- src/de/hotware/hotsound/examples/SimpleMicroPhonePlayer.java
+++ src/de/hotware/hotsound/examples/SimpleMicroPhonePlayer.java
@@ -2,7 +2,9 @@ package de.hotware.hotsound.examples;
 
 import java.net.MalformedURLException;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.Mixer;
@@ -21,6 +23,7 @@ public class SimpleMicroPhonePlayer {
 	public static void main(String[] args) throws MusicPlayerException, MalformedURLException, InterruptedException, LineUnavailableException {
 			List<Mixer> mixers = RecordAudio.getRecordMixers();
 			if(mixers.size() > 0) {
+				ExecutorService service = Executors.newSingleThreadExecutor();
 				IMusicPlayer player = new StreamMusicPlayer(new IMusicListener() {
 
 					@Override
@@ -28,7 +31,7 @@ public class SimpleMicroPhonePlayer {
 						System.out.println("stopped");
 					}
 					
-				}, Executors.newSingleThreadExecutor());
+				}, service);
 				Mixer mixer = mixers.get(0);
 				mixer.open();
 				player.insert(new RecordSong(mixer), new BasicAudioDevice());
@@ -36,6 +39,8 @@ public class SimpleMicroPhonePlayer {
 				//wait 10 seconds (equals approx. 10 seconds of saved audio)
 				Thread.sleep(10000);
 				player.stop();
+				service.shutdown();
+				service.awaitTermination(1000, TimeUnit.MILLISECONDS);
 			}
 	}
 
