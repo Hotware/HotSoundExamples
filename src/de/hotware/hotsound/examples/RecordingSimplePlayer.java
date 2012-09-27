f83f4f114d0971fc426f8cf9bb640eec8d5f7674
Martin Braun <martinbraun123@aol.com>
2012-09-27 18:45:06 +0200

    recording example

---
+++ src/de/hotware/hotsound/examples/RecordingSimplePlayer.java
@@ -0,0 +1,48 @@
+package de.hotware.hotsound.examples;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.Mixer;
+
+import de.hotware.hotsound.audio.player.IMusicListener;
+import de.hotware.hotsound.audio.player.IMusicPlayer;
+import de.hotware.hotsound.audio.player.MusicPlayerException;
+import de.hotware.hotsound.audio.player.RecordSong;
+import de.hotware.hotsound.audio.player.StreamMusicPlayer;
+import de.hotware.hotsound.audio.data.RecordAudio;
+import de.hotware.hotsound.audio.data.RecordingAudioDevice;
+
+public class RecordingSimplePlayer {
+
+	public static void main(String[] args) throws MusicPlayerException,
+			MalformedURLException,
+			InterruptedException, LineUnavailableException {
+		List<Mixer> mixers = RecordAudio.getRecordMixers();
+		if(mixers.size() > 0) {
+			ExecutorService service = Executors.newSingleThreadExecutor();
+			IMusicPlayer player = new StreamMusicPlayer(new IMusicListener() {
+
+				@Override
+				public void onEnd(MusicEvent pEvent) {
+					System.out.println("stopped");
+				}
+				
+			}, service);
+			Mixer mixer = mixers.get(0);
+			player.insert(new RecordSong(mixer), new RecordingAudioDevice(new File("recording.wav")));
+			player.start();
+			//wait 10 seconds (equals approx. 10 seconds of saved audio)
+			Thread.sleep(10000);
+			player.stop();
+			service.shutdown();
+			service.awaitTermination(1000, TimeUnit.MILLISECONDS);
+		}
+	}
+
+}
